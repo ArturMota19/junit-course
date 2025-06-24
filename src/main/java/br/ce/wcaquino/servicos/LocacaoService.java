04b3128b6ecd549cbd951089dc5637fc3fbb24c5
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-06-24 16:32:51 -0300

    FEAT: Add java locacao service test

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,15 +1,11 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-
 import java.util.Date;
-
-import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 public class LocacaoService {
 	
@@ -31,36 +27,4 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	// Princípio FIRST
-	// FAST -> Teste deve ser executado rápido
-	// INDEPENDENT -> Não deve depender de outro teste
-	// REPEATABLE -> Pode ser executado quando e quantas vezes quiser
-	// SELF-VERIFYING -> Auto verificável, deve saber quando o teste funcionou e quando falhou
-	// TIMELY -> "Oportuno", deve ser criado no momento correto
-
-	/*
-	 	Frameworks do tipo Unit devem definir:
-		1- Test Runner -> Quem vai executar os testes e coletar resultados
-		2- Test Fixture -> Pré condições necessárias p os testes
-		3- Test Suites -> Elencar testes que serão executados
-		4- Test Result Formatter -> Organização de resultado
-		5- Assertions -> Verifica comportamento ou estado do que está sendo testado
-	*/
-
-	@Test
-
-	public void teste() {
-		// cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario X");
-		Filme filme = new Filme("Filme Z", 1, 10.0);
-		// acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-
-		// verificacao
-		System.out.println(locacao.getValor() == 10.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-
-	}
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,44 @@
+package br.ce.wcaquino.servicos;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+
+public class LocacaoServiceTest {
+
+	// Princípio FIRST
+	// FAST -> Teste deve ser executado rápido
+	// INDEPENDENT -> Não deve depender de outro teste
+	// REPEATABLE -> Pode ser executado quando e quantas vezes quiser
+	// SELF-VERIFYING -> Auto verificável, deve saber quando o teste funcionou e quando falhou
+	// TIMELY -> "Oportuno", deve ser criado no momento correto
+
+	/*
+	 	Frameworks do tipo Unit devem definir:
+		1- Test Runner -> Quem vai executar os testes e coletar resultados
+		2- Test Fixture -> Pré condições necessárias p os testes
+		3- Test Suites -> Elencar testes que serão executados
+		4- Test Result Formatter -> Organização de resultado
+		5- Assertions -> Verifica comportamento ou estado do que está sendo testado
+	*/
+
+    @Test
+    public void teste() {
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario X");
+        Filme filme = new Filme("Filme Z", 1, 10.0);
+
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        assertTrue(locacao.getValor() == 10.0);
+        assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
+        assertTrue(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)));
+    }
+}
